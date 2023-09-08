18c773b1077728aaf198de66fc309510eefbf514
Lui Gill Aquini <lgaquini@gmail.com>
2023-09-08 19:08:01 -0300

    Ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let result = 0;
+  result = (temperature - 32) * (5 / 9);
+  let aproxResult = Math.round(result * 10) / 10;
+  return aproxResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let result = 0;
+  result = temperature * (9 / 5) + 32;
+  let aproxResult = Math.round(result * 10) / 10;
+  return aproxResult;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
